78d3340bedd336ecf836fe1ed09ef7f9d794157a
AlwinJacobThomas <kailath.ajt@gmail.com>
2024-01-05 14:11:41 +0530

    made some changes in bg-position,p changed to Mama Mary,coming soon reworked and added as default route

--- src/pages/Section1.jsx
+++ src/pages/Section1.jsx
@@ -9,7 +9,7 @@ const Section1 = () => {
       <div className="heading"> 
         <h1 className="section1-title"><span>Knights</span> of the Kingdom: </h1>
         {/* <h3 className="section1-subtitle">Empowering Campuses with the Rosary</h3> */}
-        <p className="section1-desc">"Knights of the Kingdom is a passionate initiative dedicated to fostering devotion to Mother Mary and instilling the habit of praying the rosary  as a powerful weapon against the challenges of the modern world.We will be serving as modern-day knights of the Kingdom of God, carrying rosary ,the strongest weapon to our Campuses. <br /><br />Let's take the mission to spread the love and protection of Mary, building a community of prayer warriors in campuses."</p>
+        <p className="section1-desc">"Knights of the Kingdom is a passionate initiative dedicated to fostering devotion to Mother Mary and instilling the habit of praying the rosary  as a powerful weapon against the challenges of the modern world.We will be serving as modern-day knights of the Kingdom of God, carrying rosary ,the strongest weapon to our Campuses. <br /><br />Let's take the mission to spread the love and protection of Mama Mary, building a community of prayer warriors in campuses."</p>
       </div>
     
       {/* <div className="video_container" id="about-video">
