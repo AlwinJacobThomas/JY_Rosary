a843c05e7c18ec98f15471f645c4a9fb21082def
AlwinJacobThomas <kailath.ajt@gmail.com>
2024-01-07 02:09:13 +0530

    video responsiveness added

--- src/pages/Section1.jsx
+++ src/pages/Section1.jsx
@@ -13,7 +13,7 @@ const Section1 = () => {
       </div>
     
       <div className="video_container" id="about-video">
-        <iframe width="100%" height="100%"src="https://youtu.be/dj_HnYQcekc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/dj_HnYQcekc?si=2v5rtJ4xEO7Db3v4"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
       
     </div>
